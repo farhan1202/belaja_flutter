571d94257d87e9a1d3e61658c55ecb1b5db83e76
unknown <hafifi1202@gmail.com>
2021-12-14 11:10:19 +0700

    animatedContainer and gestureDetector

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,23 +12,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Random random = Random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("text style"),
-        ),
-        body: Center(
-          child: Text(
-            "ini adalah contoh text ",
-            style: TextStyle(
-                fontFamily: "Freescpt",
-                fontSize: 30,
-                decoration: TextDecoration.underline),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("animated container"),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {});
+          },
+          child: AnimatedContainer(
+            duration: Duration(seconds: 1),
+            width: 50.0 + random.nextInt(101),
+            height: 50.0 + random.nextInt(101),
+            color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256),
+                random.nextInt(256)),
           ),
         ),
       ),
-    );
+    ));
   }
 }
