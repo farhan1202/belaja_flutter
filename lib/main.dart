9c4c9032befcba8039f019806a2538973bf2599a
unknown <hafifi1202@gmail.com>
2021-12-15 15:42:17 +0700

    TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/mainPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,33 +10,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(200),
-          child: AppBar(
-            backgroundColor: Colors.amber,
-            flexibleSpace: Stack(children: [
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: Container(
-                  margin: EdgeInsets.all(30),
-                  child: Positioned(
-                    child: Text(
-                      "Appbar Racikan",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700),
-                    ),
-                  ),
+      home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("contoh tab bar"),
+              bottom: TabBar(tabs: [
+                Tab(
+                  icon: Icon(Icons.comment),
+                  text: "Comment",
                 ),
-              ),
-            ]),
-          ),
-        ),
-      ),
+                Tab(
+                  child: Image(
+                      image: NetworkImage(
+                          "https://cdn.myanimelist.net/images/characters/7/441094.jpg")),
+                ),
+                Tab(
+                  icon: Icon(Icons.person),
+                ),
+                Tab(
+                  icon: Icon(Icons.lock),
+                )
+              ]),
+            ),
+            body: TabBarView(
+              children: [
+                Center(
+                  child: Text("ini tab 1"),
+                ),
+                Center(
+                  child: Text("ini tab 2"),
+                ),
+                Center(
+                  child: Text("ini tab 3"),
+                ),
+                Center(
+                  child: Text("ini tab 4"),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
