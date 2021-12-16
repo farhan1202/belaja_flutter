e61e2e532bc67cee1d7b7fd7337a5277102dd78f
unknown <hafifi1202@gmail.com>
2021-12-16 13:49:29 +0700

    Font Features (Flutter 1.7)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application/Colorful_button.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -19,23 +21,54 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Gradient Opacity"),
+          title: Text("featured font"),
         ),
         body: Center(
-          child: ShaderMask(
-            shaderCallback: (bounds) {
-              return LinearGradient(
-                      colors: [Colors.black, Colors.transparent],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter)
-                  .createShader(
-                      Rect.fromLTRB(0, 0, bounds.width, bounds.height));
-            },
-            blendMode: BlendMode.dstIn,
-            child: Image(
-                width: 300,
-                image: NetworkImage(
-                    "https://cdn.myanimelist.net/images/characters/7/441094.jpg")),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "contoh text 1",
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                "contoh text 2 small caps",
+                style: TextStyle(
+                    fontSize: 20, fontFeatures: [FontFeature.enable("smcp")]),
+              ),
+              Text(
+                "contoh text 1/2 small caps & frag",
+                style: TextStyle(fontSize: 20, fontFeatures: [
+                  FontFeature.enable("smcp"),
+                  FontFeature.enable("frac")
+                ]),
+              ),
+              Text(
+                "contoh cardo  tanpa apapun",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: "Cardo",
+                ),
+              ),
+              Text(
+                "contoh text 1/2 small caps & frag",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: "Cardo",
+                    fontFeatures: [FontFeature.oldstyleFigures()]),
+              ),
+              Text(
+                "Milonga regular",
+                style: TextStyle(fontSize: 20, fontFamily: "Milonga"),
+              ),
+              Text(
+                "Milonga regular styl set 5",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: "Cardo",
+                    fontFeatures: [FontFeature.stylisticSet(15)]),
+              ),
+            ],
           ),
         ),
       ),
