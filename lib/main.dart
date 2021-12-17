a039157a7b0a4a75fd4bdc7373381cf0124b3300
unknown <hafifi1202@gmail.com>
2021-12-17 21:51:34 +0700

    Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,77 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_application/colorBloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isStop = true;
+  bool isBlack = true;
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home:
-            BlocProvider(create: (context) => ColorBloc(), child: HomePage()));
-  }
-}
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Timer"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                counter.toString(),
+                style: TextStyle(
+                    fontSize: 50, color: (isBlack) ? Colors.black : Colors.red),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Timer(Duration(seconds: 5), () {
+                    setState(() {
+                      isBlack = !isBlack;
+                    });
+                  });
+                },
+                child: Text("Ubah warna 5 detik kemudian"),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Timer.run(() {
+                    setState(() {
+                      isBlack = !isBlack;
+                    });
+                  });
+                },
+                child: Text("Ubah warna langsung"),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  counter = 0;
+                  isStop = false;
+                  Timer.periodic(Duration(seconds: 1), (timer) {
+                    if (isStop) timer.cancel();
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    ColorBloc bloc = BlocProvider.of<ColorBloc>(context);
-    return Scaffold(
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              bloc.add(ColorEvent.toAmber);
-            },
-            backgroundColor: Colors.amber,
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              bloc.add(ColorEvent.toLightBlue);
-            },
-            backgroundColor: Colors.lightBlue,
-          )
-        ],
-      ),
-      appBar: AppBar(
-        title: Text("Bloc dengan pluggin"),
-      ),
-      body: Center(
-        child: BlocBuilder<ColorBloc, Color>(
-          builder: (context, state) => AnimatedContainer(
-            duration: Duration(milliseconds: 500),
-            color: state,
-            width: 150,
-            height: 150,
+                    setState(() {
+                      counter++;
+                    });
+                  });
+                },
+                child: Text("start timer"),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  isStop = true;
+                },
+                child: Text("stop timer"),
+              ),
+            ],
           ),
         ),
       ),
