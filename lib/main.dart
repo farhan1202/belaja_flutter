dc8fef7e28b6e21c87a726ec56a3042269ed5864
unknown <hafifi1202@gmail.com>
2021-12-17 21:15:40 +0700

    BLoC State Management (flutter_bloc Package)

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/color_bloc.dart';
+import 'package:flutter_application/colorBloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ColorBloc bloc = ColorBloc();
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    bloc.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                bloc.eventSink.add(ColorEvent.to_amber);
-              },
-              backgroundColor: Colors.amber,
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                bloc.eventSink.add(ColorEvent.to_light_blue);
-              },
-              backgroundColor: Colors.blue,
-            )
-          ],
+        home:
+            BlocProvider(create: (context) => ColorBloc(), child: HomePage()));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ColorBloc bloc = BlocProvider.of<ColorBloc>(context);
+    return Scaffold(
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              bloc.add(ColorEvent.toAmber);
+            },
+            backgroundColor: Colors.amber,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              bloc.add(ColorEvent.toLightBlue);
+            },
+            backgroundColor: Colors.lightBlue,
+          )
+        ],
+      ),
+      appBar: AppBar(
+        title: Text("Bloc dengan pluggin"),
+      ),
+      body: Center(
+        child: BlocBuilder<ColorBloc, Color>(
+          builder: (context, state) => AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            color: state,
+            width: 150,
+            height: 150,
+          ),
         ),
-        appBar: AppBar(
-          title: Text("Bloc tanpa librari"),
-        ),
-        body: Center(
-            child: StreamBuilder(
-          stream: bloc.stateStream,
-          initialData: Colors.amber,
-          builder: (context, snapshot) {
-            return AnimatedContainer(
-              duration: Duration(milliseconds: 500),
-              width: 100,
-              height: 100,
-              color: snapshot.data,
-            );
-          },
-        )),
       ),
     );
   }
