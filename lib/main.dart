1f7e4ad6ccd530b4a0feb567d639b86d0380c2ad
unknown <hafifi1202@gmail.com>
2021-12-17 10:44:14 +0700

    Shared Preferences & Double Question Mark Operator (??)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,79 +13,71 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  double myPadding = 5;
+  TextEditingController controller = TextEditingController(text: "");
+  bool sOne = false;
+
+  void saveData() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString("nama", controller.text);
+    pref.setBool("switch", sOne);
+  }
+
+  Future<String> getNama() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getString("nama") ?? "No Name";
+  }
+
+  Future<bool> getSwitch() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getBool("switch") ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("animated padding"),
+          title: Text("shared preferend"),
         ),
-        body: Column(
-          children: [
-            Flexible(
-                flex: 1,
-                child: Row(
-                  children: [
-                    Flexible(
-                        flex: 1,
-                        child: AnimatedPadding(
-                          duration: Duration(seconds: 1),
-                          padding: EdgeInsets.all(myPadding),
-                          child: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                myPadding = 20;
-                              });
-                            },
-                            child: Container(
-                              color: Colors.red,
-                            ),
-                          ),
-                        )),
-                    Flexible(
-                        flex: 1,
-                        child: AnimatedPadding(
-                          duration: Duration(seconds: 1),
-                          padding: EdgeInsets.all(myPadding),
-                          child: Container(
-                            color: Colors.green,
-                          ),
-                        ))
-                  ],
-                )),
-            Flexible(
-                flex: 1,
-                child: Row(
-                  children: [
-                    Flexible(
-                        flex: 1,
-                        child: AnimatedPadding(
-                          duration: Duration(seconds: 1),
-                          padding: EdgeInsets.all(myPadding),
-                          child: Container(
-                            color: Colors.blue,
-                          ),
-                        )),
-                    Flexible(
-                        flex: 1,
-                        child: AnimatedPadding(
-                          duration: Duration(seconds: 1),
-                          padding: EdgeInsets.all(myPadding),
-                          child: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                myPadding = 5;
-                              });
-                            },
-                            child: Container(
-                              color: Colors.yellow,
-                            ),
-                          ),
-                        ))
-                  ],
-                ))
-          ],
+        body: Center(
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  controller: controller,
+                ),
+                Switch(
+                  value: sOne,
+                  onChanged: (value) {
+                    setState(() {
+                      sOne = value;
+                    });
+                  },
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    saveData();
+                  },
+                  child: Text("Save"),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      getNama().then((value) {
+                        controller.text = value;
+                      });
+                      getSwitch().then((value) {
+                        sOne = value;
+                      });
+                    });
+                  },
+                  child: Text("Load"),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
