51364a02baf3e5f9d147318aff6c39fb9c8179a1
unknown <hafifi1202@gmail.com>
2021-12-17 22:29:52 +0700

    Custom Progress Bar (Timer + Provider)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -12,69 +12,106 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool isStop = true;
-  bool isBlack = true;
-  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Timer"),
+          title: Text("custome progress bar"),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                counter.toString(),
-                style: TextStyle(
-                    fontSize: 50, color: (isBlack) ? Colors.black : Colors.red),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  Timer(Duration(seconds: 5), () {
-                    setState(() {
-                      isBlack = !isBlack;
-                    });
-                  });
-                },
-                child: Text("Ubah warna 5 detik kemudian"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  Timer.run(() {
-                    setState(() {
-                      isBlack = !isBlack;
-                    });
-                  });
-                },
-                child: Text("Ubah warna langsung"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  counter = 0;
-                  isStop = false;
-                  Timer.periodic(Duration(seconds: 1), (timer) {
-                    if (isStop) timer.cancel();
-
-                    setState(() {
-                      counter++;
-                    });
-                  });
-                },
-                child: Text("start timer"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  isStop = true;
-                },
-                child: Text("stop timer"),
-              ),
-            ],
+          child: ChangeNotifierProvider<TimeState>(
+            create: (context) => TimeState(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<TimeState>(
+                  builder: (context, values, child) => CustomProgressBar(
+                    width: 200,
+                    value: values.time,
+                    totalValue: 15,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Consumer<TimeState>(
+                  builder: (context, value, child) => RaisedButton(
+                    onPressed: () {
+                      Timer.periodic(Duration(seconds: 1), (timer) {
+                        if (value.time == 0) {
+                          timer.cancel();
+                        } else {
+                          value.time -= 1;
+                        }
+                      });
+                    },
+                    child: Text("Start"),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+}
+
+class CustomProgressBar extends StatelessWidget {
+  final double width;
+  final int value;
+  final int totalValue;
+
+  CustomProgressBar({this.width, this.value, this.totalValue});
+  @override
+  Widget build(BuildContext context) {
+    double ration = value / totalValue;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.timer),
+        SizedBox(
+          width: 15,
+        ),
+        Stack(
+          children: [
+            Container(
+              width: width,
+              height: 10,
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(5)),
+            ),
+            Material(
+              borderRadius: BorderRadius.circular(5),
+              elevation: 3,
+              child: AnimatedContainer(
+                height: 10,
+                width: width * ration,
+                duration: Duration(milliseconds: 500),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: (ration < 0.3)
+                        ? Colors.red
+                        : (ration < 0.6)
+                            ? Colors.yellow
+                            : Colors.lightGreen),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class TimeState with ChangeNotifier {
+  int _time = 15;
+
+  int get time => _time;
+  set time(int newTime) {
+    _time = newTime;
+    notifyListeners();
   }
 }
