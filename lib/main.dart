4e59460d7615e89ddf2b9bab999b988bf8a78fb9
unknown <hafifi1202@gmail.com>
2021-12-21 12:38:00 +0700

    GetX Change Theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       initialRoute: Routes.home,
       getPages: AppPage.pages,
